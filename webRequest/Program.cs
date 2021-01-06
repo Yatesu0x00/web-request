fe7fa2f021de54fe73281c6dabddba88befaaaa0
Yatesu0x00 <61599866+Yatesu0x00@users.noreply.github.com>
2021-01-06 14:57:49 +0100

    Add project files.

---
+++ webRequest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace webRequest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            
+            //var url = "http://localhost:51614/Home/ItWorks";
+            var url = "http://localhost:51614/home/test3";
+            HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(url);
+
+            if(httpResponseMessage.StatusCode == System.Net.HttpStatusCode.OK) 
+            {
+                var content = httpResponseMessage.Content;
+                var data = await content.ReadAsStringAsync();
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
