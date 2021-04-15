bb69a71a703706d57443612dacf84b7cd9c957d9
Yatesu0x00 <61599866+Yatesu0x00@users.noreply.github.com>
2021-04-15 14:25:32 +0200

    Added: Timer, get real time and date while waiting for response

---
+++ TestTimer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Timers;
+
+namespace TestTimer
+{
+    class Program
+    {
+        static int seconds = 0;
+        public static void Main()
+        {
+            Timer aTimer = new Timer();
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            aTimer.Interval = 1000;
+            aTimer.Enabled = true;
+
+            Console.WriteLine("Press \'q\' to quit the sample.");
+            if (Console.Read() == 'q')
+            {
+
+                Console.WriteLine("du opfer"); 
+            };
+        }
+
+        // Specify what you want to happen when the Elapsed event is raised.
+        private static void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            Console.WriteLine("test");
+            seconds++;
+        }
+    }
+}
--- webRequest/Program.cs
+++ webRequest/Program.cs
@@ -1,24 +1,58 @@
 ﻿using System;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace webRequest
 {
     class Program
     {
+        static int seconds = 0;
+        static int number = 0;
+        
         static async Task Main(string[] args)
         {
+            //Timer
+            System.Timers.Timer aTimer = new System.Timers.Timer();
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            aTimer.Interval = 1000;
+            aTimer.Enabled = true;
+
             var httpClient = new HttpClient();
-            
-            var url = "http://172.29.213.9:32000/home/test3";
+
+            var url = "https://localhost:44344/home/testsamenode"; //172.29.213.18:32001/home/testanothernode                                                     
+
             HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(url);
 
-            if(httpResponseMessage.StatusCode == System.Net.HttpStatusCode.OK) 
-            {
+            if (httpResponseMessage.StatusCode == System.Net.HttpStatusCode.OK)
+            {               
                 var content = httpResponseMessage.Content;
                 var data = await content.ReadAsStringAsync();
-                Console.WriteLine(data);
+
+                Console.WriteLine("\n" + "[" + seconds + "sec]" +"Response: " + data);
+                aTimer.Enabled = false;
+            }                                     
+        }
+
+        private static void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            seconds++;
+            number++;
+
+            //Return time in 24h format 
+            var time24 = DateTime.Now.ToString("HH:mm:ss");                     
+            DateTime now = DateTime.Now;
+
+            string strDate = now.ToString("yyyy-MM-dd"); //Return current date in yyyy-MM-dd format
+
+            Console.WriteLine("| "+ number + "sec |" + " " + time24 + " | " + strDate + " |");                     
+            string blank = "--------------------------------";           
+            if (seconds >= 10)
+            {
+                blank = "---------------------------------";
             }
+            Console.WriteLine(blank);                    
         }
     }
 }
